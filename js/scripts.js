cc1f0b83a08ea6f086a7a16895ea10a0507987b3
BrouWilliam <brouwilliam@hotmail.com>
2021-10-14 22:44:25 -0300

    campeonato e noticias

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let swiperPortfolio = new Swiper(".containerzada", {
+    cssMode: false,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-pagination",
+      prevEl: ".swiper-pagination",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
